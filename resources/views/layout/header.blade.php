c56ff97919bc4fae389659a3795da56251202959
Muhammadyor <jorayev.m96@gmail.com>
2020-04-29 17:11:04 +0500

    header and main

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,1 +1,14 @@
 // header
+<div class="topnav">              
+  <div class="search-container">
+    <form>
+      <input type="text" placeholder="Search for...">
+      <button type="submit"></button>
+    </form>
+  </div>
+  <div class="menu">
+    <a id="events" href="#events">Events</a>
+    <a id="about" href="#about">About</a>
+    <a id="sign" href="#sign">Sign</a>
+  </div>
+</div>
