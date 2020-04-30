53fa82704cb286d65a3694a3381f4b7d1180bf91
Akhmadjon <axmad.sanaqulov@users.noreply.github.com>
2020-04-30 16:44:01 +0500

    Add footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,1 +1,24 @@
-// footer
+<footer>
+        <div class="menu_footer">
+            <ul class="menu_list">
+                <li><a href="#">Main</a></li>
+                <li><a href="#">Events</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Register</a></li>
+                <li><a href="#">Sign in</a></li>
+            </ul>
+        </div>
+            <div class="social_footer">
+                <P>Visit us on:</P>
+                <div class="icon_footer">
+                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+                    <li><a href="#"><i class="fab fa-vk"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </div>
+            </div>
+            <div class="adress_footer">
+                <a>18 Afrosiab Street, Tashkent, Uzbekistan</a>
+                <li><a href="#">Events@gmail.com</a></li>
+            </div>
+</footer>
